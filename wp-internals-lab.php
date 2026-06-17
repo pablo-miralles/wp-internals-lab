0115d1853b73eaf047f105fd83ee188a43958113
Pablo Miralles <pablo@mowomo.com>
2026-06-17 18:10:47 +0200

    Added plugin header in the main file

---
+++ wp-internals-lab.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: WP Internals Lab
+Description: Plugin used as playground for actions and hooks
+Version: 0.1.0
+Author: Pablo Miralles
+Author URI: https://github.com/pablo-miralles
+Text Domain: wp-internals-lab
+Domain Path: /languages
+*/
